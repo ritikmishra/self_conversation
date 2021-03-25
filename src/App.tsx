31a08588c8962c3198dfaabe30266200fb9713c4
Ritik Mishra <ritik.mishra314@gmail.com>
2021-03-24 20:52:12 -0500

    really basic functionality
    - add eslint
    - remove the default junk
    - add zangodb for interfacing with indexed db

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import zango from "zangodb";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Message {
+    _id: string;
+
+    /** Unix epoch timestamp, milliseconds */
+    timestamp: number;
+
+    /** If this is the user on the left side of the screen */
+    left: boolean;
+
+    /** The content of this message */
+    text: string;
 }
+
+const getCollection = () => {
+    const db = new zango.Db("textdb", 0, { messages: ["left"] });
+    const messagesCollection = db.collection("messages");
+    return messagesCollection;
+};
+
+const createId = (): string => {
+    return "xxxxxxxxxxxxxx".replace(/x/ig, () => Math.floor(Math.random() * 36).toString(36));
+};
+
+const App: React.FC = () => {
+    const [messagesCollection] = useState(getCollection());
+    const [messages, setMessages] = useState<Required<Message[]>>([]);
+    const [textBoxMessage, setTextBoxMessage] = useState<string>("");
+
+    const addNewMessageToCollection = async (messageText: string): Promise<void> => {
+        const newMessage: Message = {
+            _id: createId(),
+            timestamp: Date.now(),
+            left: messages.length % 2 === 0,
+            text: messageText
+        };
+        await messagesCollection.insert(newMessage);
+        setMessages([...messages, newMessage]);
+    };
+
+    return (
+        <div className="App">
+            <div className="submitArea">
+                {messages.map((message, i) => <p key={message._id} className={i % 2 === 0 ? "leftMessage" : "rightMessage"}>{message.text}</p>)}
+
+
+                <textarea placeholder="What are you going to say to yourself?" onChange={event => setTextBoxMessage(event.target.value)}>
+
+                </textarea>
+                <button onClick={() => addNewMessageToCollection(textBoxMessage)}>Add Message</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
